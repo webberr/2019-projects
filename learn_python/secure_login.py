0371343351230aec06c30edefe8878f3bff1e401
webberr <webberr@gmail.com>
2019-02-14 20:30:35 -0800

    More Python examples

---
+++ learn_python/secure_login.py
@@ -0,0 +1,13 @@
+import os
+import logging
+
+passwordFile = open('SecretPasswordFile.txt')
+secretPassword = passwordFile.read()
+print('Enter your password.')
+typedPassword = input() 
+if typedPassword == secretPassword:
+    print('Access granted')
+    if typedPassword == '12345':
+        print('That password is one that an idiot puts on their luggage.')
+    else:
+        print('Access denied')
