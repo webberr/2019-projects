b69e08d3f1c76dd4dc99bf205158ba191b069246
webberr <webberr@gmail.com>
2019-02-22 15:06:45 -0800

    ch5 automate the boring things part 2 final

--- learn_python/addInventory.py
+++ learn_python/addInventory.py
@@ -1,1 +1,24 @@
 # addInventory.py
+def displayInventory(inventory):
+    print("Inventory:")
+    item_total = 0
+    for k, v in inventory.items():
+        item_total = item_total + v
+        print(str(v) + ' ' + k)
+    print("Total number of items: " + str(item_total))
+
+
+def addToInventory(inventory, addedItems):
+    # if the dictionary is not empty iterate values in the list to compare against the dictionary
+    # when a list item is equal to a key increment the value in the dictionary by the count in the list 
+    for i in range(len(addedItems)):
+        if(addedItems[i] in list(inventory.keys())):
+            inventory[addedItems[i]] = inventory[addedItems[i]] + 1 
+        else:
+            inventory.setdefault(addedItems[i], 1)
+    return inventory
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
