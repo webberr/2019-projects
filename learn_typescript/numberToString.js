ed11bb873b80e6b84dab885e98a8ac99723f2c75
webberr <webberr@gmail.com>
2019-02-23 10:11:38 -0800

    codewars example updates

---
+++ learn_typescript/numberToString.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+function numberToString(num) {
+    return String(num);
+}
+exports.numberToString = numberToString;
+var strVal = numberToString(5);
+console.log(strVal);
---
+++ learn_typescript/numberToString.ts
@@ -0,0 +1,6 @@
+export function numberToString(num: number): string {
+    return String(num);
+}
+
+var strVal: String  = numberToString(5);
+console.log(strVal);
