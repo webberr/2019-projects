a418028fc1d668caf82076e4f8b61634a38b2209
webberr <webberr@gmail.com>
2019-02-24 17:29:23 -0800

    Monster bash main file updates

--- learn_python/monster-bash-game/monsterBash.py
+++ learn_python/monster-bash-game/monsterBash.py
@@ -1,12 +1,22 @@
 import os
 import character
+import monster
+import game
 
 def start_game():
     #
+    print("********************************************************")
+    for i in range(2):
+        print("**                                                    **")
+    print("**                   Monster Bash                     **")
+    for i in range(2):
+        print("**                                                    **")
+    print("********************************************************")
 
 def main():
+    #
     if os.path.exists('character.dat'):
-        start_game(os.open(character.dat))
+        start_game(os.open('character.dat'))
     else:
         new_char()
 
