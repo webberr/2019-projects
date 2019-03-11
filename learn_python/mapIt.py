6e3ac13df33e7951ff8ffd97d23cb8d5b471f760
webberr <webberr@gmail.com>
2019-03-10 19:44:45 -0700

    mapIt changes

--- learn_python/mapIt.py
+++ learn_python/mapIt.py
@@ -1,2 +1,10 @@
+# This is what your program does:
+# Gets a street address from the command line arguments or clipboard.
+# Opens the web browser to the Google Maps page for the address.
+# This means your code will need to do the following:
+# Read the command line arguments from sys.argv.
+# Read the clipboard contents.
+# Call the webbrowser.open() function to open the web browser.
+
 import webbrowser
 
